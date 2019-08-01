109899039e62fa90df0bbd2a9fafc9cdbd805c5c
Jonathan Fitch <jfitch1@ymail.com>
2019-08-01 10:49:00 -0400

    Fetching data and showing titles successful except Popular tab.

--- app/src/main/java/com/mathgeniusguide/project7/fragments/News.kt
+++ app/src/main/java/com/mathgeniusguide/project7/fragments/News.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.mathgeniusguide.project7.R
 import com.mathgeniusguide.project7.util.Constants
 import com.mathgeniusguide.project7.util.OnSwipeTouchListener
 import com.mathgeniusguide.project7.viewmodel.NewsViewModel
+import kotlinx.android.synthetic.main.news.*
 
 class News: Fragment() {
     lateinit var viewModel: NewsViewModel
@@ -42,12 +44,36 @@ class News: Fragment() {
         when (n) {
             Constants.TOP_NEWS -> {
                 viewModel.fetchTopNews()
+                viewModel.topNews?.observe(viewLifecycleOwner, Observer {
+                    if (it != null) {
+                        val list = it.results
+                        for (i in list) {
+                            news_test.append("\n${i.title}")
+                        }
+                    }
+                })
             }
             Constants.POPULAR_NEWS -> {
                 viewModel.fetchPopularNews()
+                viewModel.popularNews?.observe(viewLifecycleOwner, Observer {
+                    if (it != null) {
+                        val list = it.results
+                        for (i in list) {
+                            news_test.append("\n${i.title}")
+                        }
+                    }
+                })
             }
             Constants.POLITICS_NEWS -> {
                 viewModel.fetchPoliticsNews()
+                viewModel.politicsNews?.observe(viewLifecycleOwner, Observer {
+                    if (it != null) {
+                        val list = it.results
+                        for (i in list) {
+                            news_test.append("\n${i.title}")
+                        }
+                    }
+                })
             }
         }
     }
--- app/src/main/java/com/mathgeniusguide/project7/responses/popular/PopularMedia.kt
+++ app/src/main/java/com/mathgeniusguide/project7/responses/popular/PopularMedia.kt
@@ -4,7 +4,7 @@ data class PopularMedia(
     val approved_for_syndication: Int,
     val caption: String,
     val copyright: String,
-    val media-metadata: List<PopularMediaMetadata>,
+    val mediaMetadata: List<PopularMediaMetadata>,
     val subtype: String,
     val type: String
 )
